8d143f78be081429cc87030233a2bbc4ad20fab1
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-04 11:39:57 -0600

    v0.1 - Planning Layout 2
    Finished planning out the layout; now I need to clean it up and figure out how I'm going to do the panes...

--- siege6.lua
+++ siege6.lua
@@ -1,19 +1,29 @@
 
-o = 1
+o = 3
+
 function _draw()
     cls(4)
 
-    if btnp(4) then o += 1 end
-    if btnp(5) then o -= 1 end
-
+    
     canvas_size = 10
 
     ox = 5 oy = 5 m = o
     rectfill(ox,oy, 2 + (m * canvas_size) + ox, 2 + (m * canvas_size) + oy, 3)
     rect(ox,oy, 2 + (m * canvas_size) + ox, 2 + (m * canvas_size) + oy, 0)
 
-    ox = 5 oy = (m * canvas_size) + (2 * ox) m = 11 - m
+    ox = 5 oy = 1 + (m * canvas_size) + (2 * ox) m = 11 - m
     rectfill(ox,oy,2 + (m * canvas_size) + ox, 2 + (m * canvas_size) + oy,3)
     rect(ox,oy,2 + (m * canvas_size) + ox, 2 + (m * canvas_size) + oy, 0)
+    
 
+    rectfill(91, 5, 123, 123,3)
+    rect(91, 5, 123, 123, 0)
+
+    rectfill(41, 5, 87, 37,3)
+    rect(41, 5, 87, 37, 0)
+    
+    // ⬇️⬆️⬅️➡️ fo. future reference
+
+
+    print("start", 98, 15, 0)
 end
